69848912025cd47154627c3beb93c1839e3ac4cc
ashutoshtiwari3309 <ashutosh.tiwari@squareboat.com>
2020-08-08 14:30:56 +0530

    demo testing commit

---
+++ InitialTesting.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+
+public class InitialTesting {
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int alpha = sc.nextInt();
+		System.out.println(alpha+20);
+
+	}
+	
+}
